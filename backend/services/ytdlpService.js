64d0a6a598d64a997df5f20de4c6131f4c352be4
imsurajx <surajxcode@gmail.com>
2025-11-22 14:38:11 +0530

    feat: implemented downloadStream service using spawn for streaming downloads - Added downloadStream service method using spawn() - Streams yt-dlp output (stdout) directly to the client - Supports direct browser downloads with progress bar and save dialog - Prepared backend for full streaming-based download workflow
    Signed-off-by: imsurajx <surajxcode@gmail.com>

--- backend/services/ytdlpService.js
+++ backend/services/ytdlpService.js
@@ -7,7 +7,8 @@
  */
 
 // Importing exec from Node's child_process module
-const {exec} = require("child_process");
+const {exec, spawn} = require("child_process");
+const { format } = require("path");
 
 // Function to fetch media information from yt-dlp
 const fetchInfo = (url) => {
@@ -35,6 +36,26 @@ const fetchInfo = (url) => {
     })
 }
 
+// Function: Stream media direclty using yt-dlp
+const downloadStream = (url, format) => {
+    // Step 1: Create yt-dlp arguments
+    // -f <format> : which format to download (18, 22, 140, etc.)
+    // url         : video URL (YouTube, etc.)
+    // -o -        : output to stdout (streaming)
+    const args = [
+        "-f", format, // Selected format.
+        url,          // Video URL
+        "-o", "-"     // "-" means output to stdout (stream)
+    ];
+    // Step 2: Spawn yt-dlp
+    // spawn starts yt-dlp as a child process and gives us a stream of data (stdout)
+    const process = spawn("yt-dlp", args);
+
+    // Step 3: Return the child_process so controller can pipe it's output
+    return process;
+}
+
 module.exports = {
-    fetchInfo
+    fetchInfo,
+    downloadStream
 };
